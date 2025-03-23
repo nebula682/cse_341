b945044d4315e3294052985c5a88606954a4bdb3
nebula682 <122559277+nebula682@users.noreply.github.com>
2025-03-23 15:17:14 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const mongodb = require('./data/database');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use('/', require('./routes'));
+
+
+mongodb.initDb((err) => {
+    if (err) {
+        console.log('There is an error',err);
+    }
+     else {
+        app.listen(port, () =>
+             { console.log(`Database is listening and node Running on port ${port}`);
+        });
+    }
+});
